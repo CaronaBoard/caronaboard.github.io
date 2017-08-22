2f5e6207895d8de5bcc9bc4aff853fe088cb78e7
Circle CI <circle-ci@caronaboard.com>
2017-08-22 22:01:24 +0000

    Deploy to GitHub Pages: 409065e9e4e1a5103f1255ba417267d4cb0daa69

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,7 +2,7 @@ importScripts("https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js");
 
 firebase.initializeApp({
-  messagingSenderId: "617045704123"
+  messagingSenderId: "1026486772811" || "617045704123"
 });
 
 firebase.messaging();
